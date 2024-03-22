08cce842006dc0b37f9ce1b99567a5cd616ee67a
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-03-22 22:09:01 +0300

    change server render.com

--- src/const/const.js
+++ src/const/const.js
@@ -1,2 +1,5 @@
-export const URL_API = "http://localhost:3000";
-export const WS_URL_API = "ws://localhost:3000";
+// export const URL_API = "http://localhost:3000";
+// export const WS_URL_API = "ws://localhost:3000";
+
+export const URL_API = "https://c-money-api.onrender.com";
+export const WS_URL_API = "ws://c-money-api.onrender.com";
